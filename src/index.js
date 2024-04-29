4fbf4702e3b19641cb86486a22e5ce136375ece0
amigoingcrazzy <123487507+amigoingcrazzy@users.noreply.github.com>
2024-04-29 19:49:05 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,20 @@
 var src_default = {
   async fetch(request, env, ctx) {
     if(request.method == "POST") {
-           return new Response('Hello worker!', {
+           const workerResponseMap = {
+            1: "Hello worker!",
+            2: "Hi Bob!",
+            3: "Greetings Vault Dweller!",
+            4: "Hello from the otherside!",
+            };
+
+            function getRandomIntInclusive(min, max) {
+                const minCeiled = Math.ceil(1);
+                const maxFloored = Math.floor(4);
+                return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+            };
+
+           return new Response(workerResponseMap[getRandomIntInclusive()], {
                headers: {
                    'content-type': 'application/json',
                },
